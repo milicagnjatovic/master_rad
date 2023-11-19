7f300c6378ec026787972b24b89edcf4173565d9
Milica Gnjatovic <milicagnjatovic18@gmail.com>
2023-11-19 22:34:56 +0100

    Added function for retrieving solution.

--- grader/src/main/java/org/example/SolutionController.java
+++ grader/src/main/java/org/example/SolutionController.java
@@ -6,11 +6,16 @@ import org.json.JSONObject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-@Path("/generateSolution")
+@Path("/grader")
 public class SolutionController {
     @POST
+    @Path("/generate")
     @Consumes(MediaType.APPLICATION_JSON)
     public String generateSolution(String body){
         System.out.println(body);
@@ -21,6 +26,17 @@ public class SolutionController {
             return "Error occurred: " + e.getMessage();
         }
         return "done";
+    }
+
+    @GET
+    @Path("/getSolution")
+    public String getSolution(@QueryParam("id") String id){
+        try {
+            String solution = new String(Files.readAllBytes(Paths.get("results", id)), StandardCharsets.UTF_8);
+            return solution;
+        } catch (IOException e) {
+            return "Solution not found";
+        }
     }
 
 }
