ff5539d622bbd2b53e7b2936debbc2af3d231606
Milica Gnjatovic <milicagnjatovic18@gmail.com>
2023-12-11 10:51:50 +0100

    Error-proofing check_solution.sh

--- grader/scripts/check_solution.sh
+++ grader/scripts/check_solution.sh
@@ -7,20 +7,22 @@ user_path="student_results/$1"
 solution_path="results/$2"
 query="$3"
 
-
 #echo "$query"
 #echo "$user_path"
 #echo "$solution_path"
 
 db2 connect to stud2020 > /dev/null
 
-if db2 "$query" > "$user_path"; then
-  diff -bB "$user_path" "$solution_path" | head -n 10
+
+db2 "$query" > "$user_path" || echo "error"
+
+if [ ! -e "$solution_path" ] || [ ! -e "$user_path" ]
+then
+    echo "User file or result file is missing"
 else
-  echo "error"
+  diff -bB "$user_path" "$solution_path" | head -n 10
 fi
 
-#diff -bB "$user_path" "$solution_path" | head -n 10
 
 db2 connect reset > /dev/null
 rm "$user_path"
