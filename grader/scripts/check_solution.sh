b985995a7463de3dbd7334eb86cbbc801da582aa
Milica Gnjatovic <milicagnjatovic18@gmail.com>
2024-01-02 22:45:00 +0100

    Added checks for number of rows and names of columns.

--- grader/scripts/check_solution.sh
+++ grader/scripts/check_solution.sh
@@ -14,7 +14,7 @@ query="$3"
 db2 connect to stud2020 > /dev/null
 
 
-db2 "$query" > "$user_path"
+db2 "$query" | sed '/^$/d' > "$user_path"
 
 if [ $? -ne 0 ]; then
   if [ ! -e "$user_path"  ]; then
@@ -31,10 +31,30 @@ then
     echo "E300 | result file is missing"
     rm "$user_path"
     exit 1
-else
-  diff -bB "$user_path" "$solution_path" | head -n 10
 fi
 
+student_end=$(tail -1 "$user_path")
+solution_end=$(tail -1 "$solution_path")
+
+if [ "$student_end" != "$solution_end" ]; then
+  echo "Different number of rows"
+  rm "$user_path"
+  exit 1
+fi
+
+
+student_start=$(head -1 "$user_path")
+solution_start=$(head -1 "$solution_path")
+
+shopt -s nocasematch # case insensitive
+if [[ "$student_start" != "$solution_start" ]]; then
+  echo "Wrong column names"
+  rm "$user_path"
+  exit 1
+fi
+shopt -u nocasematch
+
+diff -bBi "$user_path" "$solution_path" | head -n 10
 
 db2 connect reset > /dev/null
 rm "$user_path"
